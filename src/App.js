7b6990dcaeb734d290af57274e5e1ec56d66adb3
Tanwi Sharma <40869991+TanwiSharma@users.noreply.github.com>
2023-12-07 12:45:18 +0530

    intial commit
    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { type } from '@testing-library/user-event/dist/type';
 function App() {
+  // Declared an array of items
+  const fruits = ["Apple", "Mango", "Banana", "GFG", "Bujinga"];
+  const check = "Running";
+  //fruits.sort();
+  //console.log(fruits);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <style>
+      
+      </style>
+        <img src={logo} className="App-logo" alt="logo" title="applogo"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -14,9 +22,43 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          aria-label={check}
+          aria-expanded="true"
+          name="label"
+          title="app logo"
+          aria-selected="true"
+          allowTabFocus = "true"
         >
           Learn React
         </a>
+        <ul>
+        {fruits.map((prg) => (<li>{prg}</li>))}
+        </ul>
+
+        <ol className="no-numbers">
+          <li><h5 aria-labelledby="visible">hi</h5></li>
+          <li aria-current="true"><a href="https://reactjs.org" role="button">hello moto</a></li>
+          <li><h5>yes</h5></li>
+        </ol>
+        
+<div className="heading-1">onlune clain</div>
+        <ul role="tablist">
+  <li role="presentation">
+    <button role="tab" href="#panel1" id="tab1" aria-selected="true">Tab one</button>
+  </li>
+  <li role="presentation">
+    <button role="tab" href="#panel2" id="tab2" aria-selected="true">Tab two</button>
+  </li>
+  <li role="presentation">
+    <button role="tab" href="#panel3" id="tab3" aria-selected="true">Tab three</button>
+  </li>
+  <li role="presentation">
+    <button role="tab" href="#panel4" id="tab4" tabindex="0" aria-selected="true">Tab four</button>
+  </li>
+</ul>
+
+<h1>=======Fetch api and display here====</h1>
+<p>Print java api fetched values</p>
       </header>
     </div>
   );
